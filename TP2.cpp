ed8b0c1155be25921df49d95db5d37a36295d3ae
mariowexd <53508927+mariowexd@users.noreply.github.com>
2021-12-09 23:37:19 -0300

    Update TP2.cpp

--- TP2.cpp
+++ TP2.cpp
@@ -16,42 +16,13 @@ int main(){
   jug->avanzarCursor();
   procesarJuego(jug, tab, c);
 
-
   jug->reiniciarCursor();
 
-  while(jug->avanzarCursor()){
-	if(jug->getCursor()->getGano()==true) felicitacion(jug->getCursor());
-  }
-
   const char a[]="file.bmp";
-  ///test bitmap
-  /*
-  const char a[] = "imagen.bmp";
-  Tablero *tablero = new Tablero(4,4,4);
-  tablero->getCasillero(1,1,1)->setId(4);
-  tablero->getCasillero(1,2,2)->setId(4);
-  tablero->getCasillero(1,1,3)->setId(4);
-  tablero->getCasillero(1,2,4)->setId(4);
-
-  tablero->getCasillero(2,4,1)->setId(5);
-  tablero->getCasillero(2,3,2)->setId(5);
-  tablero->getCasillero(2,4,3)->setId(5);
-  tablero->getCasillero(2,3,4)->setId(5);
-
-  tablero->getCasillero(3,1,1)->setId(4);
-  tablero->getCasillero(3,2,2)->setId(4);
-  tablero->getCasillero(3,3,3)->setId(4);
-  tablero->getCasillero(3,4,4)->setId(4);
-
-  tablero->getCasillero(4,4,1)->setId(5);
-  tablero->getCasillero(4,3,2)->setId(5);
-  tablero->getCasillero(4,2,3)->setId(5);
-  tablero->getCasillero(4,1,4)->setId(5);*/
 
   escribirImagen(*tab, a);
 
   delete tab;
-	
 	
   return 0;
 }
